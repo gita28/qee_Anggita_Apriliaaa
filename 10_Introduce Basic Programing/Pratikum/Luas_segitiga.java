0b00c52d41b0070ea2512391b2ebda30d44b8a1c
gita28 <anggitaaprilia2001@gmail.com>
2022-04-19 19:10:45 +0700

    comiit

---
+++ 10_Introduce Basic Programing/Pratikum/Luas_segitiga.java
@@ -0,0 +1,13 @@
+
+public class Luas_segitiga {
+    public static void main(String args[]) {
+        double alas,tinggi,luas;
+        alas = 20;
+        tinggi = 25;
+        luas = 0.5 * alas * tinggi;
+        
+        Syestem.out.println("luas segitiga adalah " +luas);
+        
+        
+    }
+}
