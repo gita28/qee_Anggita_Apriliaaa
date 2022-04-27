f3d31ca7ea692d1610aa100a95e41fa5987c8547
gita28 <81310318+gita28@users.noreply.github.com>
2022-04-27 14:05:27 +0700

    Update Perkalian.java

---
+++ 10_Introduce Basic Programing/Pratikum/Perkalian.java
@@ -0,0 +1,13 @@
+public class perkalian{
+public static void main(String[] args){
+
+System.out.println("\t Tabel Perkalian 1-9 \n");
+
+for (int i = 1; i <= 9; i++) {
+
+System.out.print(" " + i*j+ "\t);
+}
+Syestem.out.print("\n\n");
+}
+}
+}
